85927417f7ec944ccb0f08bd14a08951e0ff1829
Hoang Nguyen Dat <110254118+hoangdat251004@users.noreply.github.com>
2024-05-26 22:39:42 +0700

    Update Convert.java Convert Positive Decimal To Binary

--- change_base/Convert.java
+++ change_base/Convert.java
@@ -12,7 +12,7 @@ import java.math.BigInteger;
  * @author hoang.dat
  */
 public class Convert {
-    
+
     public String convertNumber(int baseInput, int baseOutput, String inputValue) {
         String outputValue = null;
         if (baseInput == baseOutput) {
@@ -55,19 +55,19 @@ public class Convert {
     }
 
     public String binaryToDecimal(String inputValue) {
-        BigInteger value = BigInteger.ZERO; //To save value after convert
+        BigInteger valueDec = BigInteger.ZERO; //To save value after convert
         //Take element multi with reverse index
         int startIdx = inputValue.length() - 1; //Start idx is length --> 0
         //Traversal inputValue string to multi each element
         for (int i = 0; i < inputValue.length(); i++) {
             //If matches '1' --> calc and add to value
             if (inputValue.charAt(i) == '1') {
-                value = value.add((BigInteger.valueOf(2).pow(startIdx)));
+                valueDec = valueDec.add((BigInteger.valueOf(2).pow(startIdx)));
             }
             startIdx--; //Decrease index
         }
 
-        return value.toString();
+        return valueDec.toString();
     }
 
     public String decimalToHex(String inputValue) {
@@ -77,25 +77,25 @@ public class Convert {
         // Take remainder into String from last --> 0
         while (inputDec.compareTo(new BigInteger("0")) != 0) {
             BigInteger[] divAndRemain = inputDec.divideAndRemainder(new BigInteger("16"));
-            inputDec = divAndRemain[0];
-            BigInteger remainder = divAndRemain[1];
+            inputDec = divAndRemain[0]; // update quotient
+            BigInteger remainder = divAndRemain[1]; // update remainder
             valueHex = insertRemainderIntoHexValue(remainder, valueHex);
         }
         return valueHex;
     }
 
-    public String insertRemainderIntoHexValue(BigInteger remainder, String value) {
+    public String insertRemainderIntoHexValue(BigInteger remainder, String valueHex) {
         // If remainder between [0, 9] --> insert directly 
         if (remainder.compareTo(BigInteger.ZERO) >= 0 && remainder.compareTo(new BigInteger("9")) <= 0) {
-            value = remainder.toString() + value;
-            return value;
+            valueHex = remainder.toString() + valueHex;
+            return valueHex;
         }
         //If remainder between [10, 15] --> convert to right format and insert
         if (remainder.compareTo(new BigInteger("10")) >= 0 && remainder.compareTo(new BigInteger("15")) <= 0) {
             char hexChar = (char) ('a' + remainder.intValue() - 10);
-            value = hexChar + value;
+            valueHex = hexChar + valueHex;
         }
-        return value;
+        return valueHex;
     }
 
     public String binaryToHex(String inputValue) {
@@ -103,22 +103,80 @@ public class Convert {
         return decimalToHex(decimalNum);
     }
 
-    public String decimalToBinary(String inputValue) {
+//    public String decimalToBinary(String inputValue) {
+//        String valueBinary = "";
+//        BigInteger inputDec = new BigInteger(inputValue);
+//        // Div until divisor = 0;
+//        // Take remainder into String from last --> 0
+//        while (inputDec.compareTo(new BigInteger("0")) != 0) {
+//            BigInteger[] divAndRemain = inputDec.divideAndRemainder(new BigInteger("2"));
+//            inputDec = divAndRemain[0];
+//            BigInteger remainder = divAndRemain[1];
+//            valueBinary = remainder + valueBinary;
+//        }
+//        return valueBinary;
+//    }
+    ////////////
+    public static String decimalToBinary(String inputValue) {
+        boolean isNegative = inputValue.startsWith("-");
+        String positiveValue = isNegative ? inputValue.substring(1) : inputValue;
+
+        BigInteger inputDec = new BigInteger(positiveValue);
         String valueBinary = "";
-        BigInteger inputDec = new BigInteger(inputValue);
-        // Div until divisor = 0;
-        // Take remainder into String from last --> 0
-        while (inputDec.compareTo(new BigInteger("0")) != 0) {
+
+        // Convert positive value to binary
+        while (inputDec.compareTo(BigInteger.ZERO) > 0) {
             BigInteger[] divAndRemain = inputDec.divideAndRemainder(new BigInteger("2"));
-            inputDec = divAndRemain[0];
             BigInteger remainder = divAndRemain[1];
             valueBinary = remainder + valueBinary;
+            inputDec = divAndRemain[0];
         }
+
+        // Ensure that the binary representation of zero is "0"
+        if (valueBinary.isEmpty()) {
+            valueBinary = "0";
+        }
+
+        if (isNegative) {
+            // Invert bits and add 1 to get two's complement representation
+            valueBinary = invertBits(valueBinary);
+            valueBinary = addBinary(valueBinary, "1");
+        }
+
         return valueBinary;
     }
 
+    private static String invertBits(String binary) {
+        StringBuilder inverted = new StringBuilder();
+        for (char bit : binary.toCharArray()) {
+            inverted.append(bit == '0' ? '1' : '0');
+        }
+        return inverted.toString();
+    }
+
+    private static String addBinary(String binary1, String binary2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int maxLength = Math.max(binary1.length(), binary2.length());
+
+        for (int i = 0; i < maxLength; i++) {
+            int bit1 = i < binary1.length() ? binary1.charAt(binary1.length() - 1 - i) - '0' : 0;
+            int bit2 = i < binary2.length() ? binary2.charAt(binary2.length() - 1 - i) - '0' : 0;
+            int sum = bit1 + bit2 + carry;
+            result.insert(0, sum % 2);
+            carry = sum / 2;
+        }
+
+        if (carry > 0) {
+            result.insert(0, carry);
+        }
+
+        return result.toString();
+    }
+
+    ////////////
     public String hexToDecimal(String inputValue) {
-        BigInteger value = BigInteger.ZERO; //To save value after convert
+        BigInteger valueDec = BigInteger.ZERO; //To save value after convert
         //Take element multi with reverse index
         int startIdx = inputValue.length() - 1; //Start idx is length --> 0
         //Traversal inputValue string to multi each element with 16
@@ -129,13 +187,14 @@ public class Convert {
             // else convert element to numeric data [10, 15] to calc
             BigInteger powerOfIdx = BigInteger.valueOf(16).pow(startIdx);
             if (element >= 0 && element <= 9) {
-                value = value.add(BigInteger.valueOf(element).multiply(powerOfIdx));
+                valueDec = valueDec.add(BigInteger.valueOf(element).multiply(powerOfIdx));
             } else {
-                value = value.add(BigInteger.valueOf((10 + (inputValue.charAt(i) - 'a'))).multiply(powerOfIdx));
+                int valueOfElement = (10 + (inputValue.charAt(i) - 'a'));
+                valueDec = valueDec.add(BigInteger.valueOf(valueOfElement).multiply(powerOfIdx));
             }
             startIdx--; //Decrease index
         }
-        return value.toString();
+        return valueDec.toString();
     }
 
     public String hexToBinary(String inputValue) {

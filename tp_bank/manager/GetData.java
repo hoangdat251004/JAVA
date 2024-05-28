193dcdc0e6641d51173591e93cdf106dce93136c
Hoang Nguyen Dat <110254118+hoangdat251004@users.noreply.github.com>
2024-05-28 22:01:09 +0700

    Update GetData.java

--- tp_bank/manager/GetData.java
+++ tp_bank/manager/GetData.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controller;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class GetData {
 
     public static  Scanner sc = new Scanner(System.in);
